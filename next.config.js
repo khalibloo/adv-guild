75b7b74b03f760c99b5c48049c12b45f159f410e
Khalifa Lame <khalibloo@gmail.com>
2021-10-22 02:05:55 +0400

    Press any key to start

--- next.config.js
+++ next.config.js
@@ -1,4 +1,67 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+const antdLess = require("next-plugin-antd-less");
+const pwa = require("next-pwa");
+const withPlugins = require("next-compose-plugins");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const themeOverride = lessToJS(fs.readFileSync("./src/styles/theme.less", "utf8"));
+
+module.exports = withSentryConfig(
+  withPlugins([antdLess, pwa], {
+    reactStrictMode: true,
+    // use modifyVars or lessVarsFilePath or both
+    // lessVarsFilePath: "",
+    modifyVars: { ...themeOverride },
+
+    i18n: {
+      // These are all the locales you want to support in
+      // your application
+      locales: ["en-US"],
+      // This is the default locale you want to be used when visiting
+      // a non-locale prefixed path e.g. `/hello`
+      defaultLocale: "en-US",
+    },
+
+    images: {
+      domains: ["www.gravatar.com"],
+    },
+
+    // other configs here...
+    webpack(config) {
+      // svg import support
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+      return config;
+    },
+
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }),
+  SentryWebpackPluginOptions
+);
---
+++ src/utils/config.ts
@@ -0,0 +1,13 @@
+const appEnv = process.env.NEXT_PUBLIC_APP_ENV;
+
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
+export default {
+  env: appEnv,
+  apiUri: process.env.NEXT_PUBLIC_API_URI,
+  sentryDSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+};
